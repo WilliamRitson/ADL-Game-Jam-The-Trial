a4b3656ca2d2f67acc3ad6e55316e1b56d544a09
William Ritson <william.ritson@gmail.com>
2018-10-21 10:53:30 -0700

    Made blank url redirect to start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { GameComponent } from './game/game.component';
 
 const routes: Routes = [
-  { path: 'play/:scene', component: GameComponent },
+    { path: 'play/:scene', component: GameComponent },
+    {
+        path: '',
+        redirectTo: '/play/start',
+        pathMatch: 'full'
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
